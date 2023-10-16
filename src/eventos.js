a20e7380a4ff092c84d7aee610a38604e0681044
Igorggwp <igorggwpp@gmail.com>
2023-10-16 00:25:09 -0300

    adcionando eventos dinamicos

---
+++ src/eventos.js
@@ -0,0 +1,27 @@
+const URL_EVENTOS = "https://lvdt20mj.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22eventos%22%5D%7B%0A%0A++%22imagem_evento%22%3A+imagem_evento.asset-%3Eurl%2C%0A++++%22link_evento%22%3A+link_evento%2C%0A++++%22nome_evento%22%3A+nome_evento%2C%0A%7D";
+
+// Realiza uma solicitação GET para a URL da API
+fetch(URL_EVENTOS, { method: "GET" }) 
+    .then(response => {
+        return response.json(); // Converte a resposta em formato JSON
+    })
+    .then(data => {
+        const eventosContainer = document.querySelector('.redes-img'); 
+
+        data.result.forEach(evento => { 
+            // Cria um elemento 'a' (link) para cada evento
+            const linkElement = document.createElement('a');
+            linkElement.href = evento.link_evento; 
+            linkElement.target = '_blank'; 
+
+            // Cria um elemento 'img' (imagem) para cada evento
+            const imgElement = document.createElement('img');
+            imgElement.src = evento.imagem_evento; 
+            imgElement.alt = evento.nome_evento; 
+
+            // Anexa a imagem ao link e o link ao contêiner de eventos
+            linkElement.appendChild(imgElement);
+            eventosContainer.appendChild(linkElement);
+        });
+    })
+    .catch(error => console.error("Erro ao processar a requisição:", error));
